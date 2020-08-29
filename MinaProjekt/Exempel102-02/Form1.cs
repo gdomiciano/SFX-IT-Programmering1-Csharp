cb837aae94e94ec79aea3713f4ca384c8da75211
Geisy Domiciano <geisydomiciano@gmail.com>
2020-08-29 23:07:13 +0200

    :sparkles: mitt första program

---
+++ MinaProjekt/Exempel102-02/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exempel102_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKlick_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mitt första program", "Meddelande", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
