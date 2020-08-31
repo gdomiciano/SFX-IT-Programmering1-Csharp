2d72f5d818a92e26309c50df4a1c65ef2d76b1e2
AvraamGeorgiadis <georgiadis.ab@gmail.com>
2020-08-31 22:20:12 +0200

    Upgiften 3.1 Funkar. Det fins några (updates) att göra men det är ok.

--- Inlämning-K3.Grupp3-kväll/Övning_3.1/Form1.Designer.cs
+++ Inlämning-K3.Grupp3-kväll/Övning_3.1/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtBoxHeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxBase = new System.Windows.Forms.TextBox();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(287, 253);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(242, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtBoxHeight
+            // 
+            this.txtBoxHeight.Location = new System.Drawing.Point(287, 159);
+            this.txtBoxHeight.Name = "txtBoxHeight";
+            this.txtBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxHeight.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Height";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(426, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Base";
+            // 
+            // txtBoxBase
+            // 
+            this.txtBoxBase.Location = new System.Drawing.Point(429, 159);
+            this.txtBoxBase.Name = "txtBoxBase";
+            this.txtBoxBase.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxBase.TabIndex = 4;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(287, 308);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(159, 13);
+            this.ResultLabel.TabIndex = 6;
+            this.ResultLabel.Text = "Hint: Use numbers like this, 10,5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxBase);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxHeight);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtBoxHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxBase;
+        private System.Windows.Forms.Label ResultLabel;
     }
 }
 
--- Inlämning-K3.Grupp3-kväll/Övning_3.1/Form1.cs
+++ Inlämning-K3.Grupp3-kväll/Övning_3.1/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Övning_3._1
@@ -15,6 +8,63 @@ namespace Övning_3._1
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //TODO
+            //Assume the user has given us digits only.
+            //Assume that the number is less than the max number available to ints.
+
+            //this can and will fail if its not an INT
+
+            Double triangleBase = -1;
+            Double triangleHeight = -1;
+
+            string result = "";
+
+            try
+            {
+                triangleBase = Double.Parse(txtBoxBase.Text);
+                triangleHeight = Double.Parse(txtBoxHeight.Text);
+
+                result = CalculateTriangleArea(triangleBase, triangleHeight).ToString();
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+
+
+            if (triangleBase > -1 && triangleHeight > -1)
+            {
+                UpdateResultText(result);
+            }
+            else
+            {
+                UpdateResultText("Something went wrong");
+                //TODO something went wrong.
+            }
+        }
+
+
+        /// <summary>
+        /// Updates the result field with the outcome text passed.
+        /// </summary>
+        /// <param name="message"></param>
+        void UpdateResultText(string message)
+        {
+            ResultLabel.Text = message;
+
+        }
+
+        Double CalculateTriangleArea(Double tHeight, Double tBase)
+        {
+            return (tHeight * tBase) / 2;
         }
     }
 }
