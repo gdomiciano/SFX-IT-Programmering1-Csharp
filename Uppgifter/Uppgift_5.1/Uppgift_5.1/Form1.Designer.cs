7c0c38782d54648a579d8af2c867dc079bc0265e
Geisy Domiciano <geisydomiciano@gmail.com>
2020-09-13 19:45:40 +0200

    :sparkles: skapa uppgift 5.1

---
+++ Uppgifter/Uppgift_5.1/Uppgift_5.1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Uppgift_5._1
+{
+    partial class Fönster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEtikett = new System.Windows.Forms.Label();
+            this.tbxTal = new System.Windows.Forms.TextBox();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.btnKör = new System.Windows.Forms.Button();
+            this.lblAddera1 = new System.Windows.Forms.Label();
+            this.lblMultiplicera2 = new System.Windows.Forms.Label();
+            this.lblSubtrahera6 = new System.Windows.Forms.Label();
+            this.lblHalvera = new System.Windows.Forms.Label();
+            this.lblAddera3 = new System.Windows.Forms.Label();
+            this.lblSubtraheraTalet = new System.Windows.Forms.Label();
+            this.lblFel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEtikett
+            // 
+            this.lblEtikett.AutoSize = true;
+            this.lblEtikett.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEtikett.Location = new System.Drawing.Point(39, 41);
+            this.lblEtikett.Name = "lblEtikett";
+            this.lblEtikett.Size = new System.Drawing.Size(287, 20);
+            this.lblEtikett.TabIndex = 0;
+            this.lblEtikett.Text = "Tänka på ett heltal och matta in heltalet";
+            // 
+            // tbxTal
+            // 
+            this.tbxTal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxTal.Location = new System.Drawing.Point(128, 75);
+            this.tbxTal.Name = "tbxTal";
+            this.tbxTal.Size = new System.Drawing.Size(100, 26);
+            this.tbxTal.TabIndex = 1;
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSvar.Location = new System.Drawing.Point(136, 366);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(0, 20);
+            this.lblSvar.TabIndex = 2;
+            this.lblSvar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnKör
+            // 
+            this.btnKör.Location = new System.Drawing.Point(140, 107);
+            this.btnKör.Name = "btnKör";
+            this.btnKör.Size = new System.Drawing.Size(75, 23);
+            this.btnKör.TabIndex = 4;
+            this.btnKör.Text = "Kör Magi!";
+            this.btnKör.UseVisualStyleBackColor = true;
+            this.btnKör.Click += new System.EventHandler(this.btnKör_Click);
+            // 
+            // lblAddera1
+            // 
+            this.lblAddera1.AutoSize = true;
+            this.lblAddera1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddera1.Location = new System.Drawing.Point(136, 166);
+            this.lblAddera1.Name = "lblAddera1";
+            this.lblAddera1.Size = new System.Drawing.Size(0, 20);
+            this.lblAddera1.TabIndex = 5;
+            this.lblAddera1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMultiplicera2
+            // 
+            this.lblMultiplicera2.AutoSize = true;
+            this.lblMultiplicera2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMultiplicera2.Location = new System.Drawing.Point(136, 196);
+            this.lblMultiplicera2.Name = "lblMultiplicera2";
+            this.lblMultiplicera2.Size = new System.Drawing.Size(0, 20);
+            this.lblMultiplicera2.TabIndex = 6;
+            this.lblMultiplicera2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSubtrahera6
+            // 
+            this.lblSubtrahera6.AutoSize = true;
+            this.lblSubtrahera6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtrahera6.Location = new System.Drawing.Point(136, 229);
+            this.lblSubtrahera6.Name = "lblSubtrahera6";
+            this.lblSubtrahera6.Size = new System.Drawing.Size(0, 20);
+            this.lblSubtrahera6.TabIndex = 7;
+            this.lblSubtrahera6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHalvera
+            // 
+            this.lblHalvera.AutoSize = true;
+            this.lblHalvera.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHalvera.Location = new System.Drawing.Point(136, 267);
+            this.lblHalvera.Name = "lblHalvera";
+            this.lblHalvera.Size = new System.Drawing.Size(0, 20);
+            this.lblHalvera.TabIndex = 8;
+            this.lblHalvera.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAddera3
+            // 
+            this.lblAddera3.AutoSize = true;
+            this.lblAddera3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddera3.Location = new System.Drawing.Point(136, 301);
+            this.lblAddera3.Name = "lblAddera3";
+            this.lblAddera3.Size = new System.Drawing.Size(0, 20);
+            this.lblAddera3.TabIndex = 9;
+            this.lblAddera3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSubtraheraTalet
+            // 
+            this.lblSubtraheraTalet.AutoSize = true;
+            this.lblSubtraheraTalet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtraheraTalet.Location = new System.Drawing.Point(136, 334);
+            this.lblSubtraheraTalet.Name = "lblSubtraheraTalet";
+            this.lblSubtraheraTalet.Size = new System.Drawing.Size(0, 20);
+            this.lblSubtraheraTalet.TabIndex = 10;
+            this.lblSubtraheraTalet.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFel
+            // 
+            this.lblFel.AutoSize = true;
+            this.lblFel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFel.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblFel.Location = new System.Drawing.Point(67, 137);
+            this.lblFel.Name = "lblFel";
+            this.lblFel.Size = new System.Drawing.Size(0, 18);
+            this.lblFel.TabIndex = 11;
+            // 
+            // Fönster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 408);
+            this.Controls.Add(this.lblFel);
+            this.Controls.Add(this.lblSubtraheraTalet);
+            this.Controls.Add(this.lblAddera3);
+            this.Controls.Add(this.lblHalvera);
+            this.Controls.Add(this.lblSubtrahera6);
+            this.Controls.Add(this.lblMultiplicera2);
+            this.Controls.Add(this.lblAddera1);
+            this.Controls.Add(this.btnKör);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.tbxTal);
+            this.Controls.Add(this.lblEtikett);
+            this.Name = "Fönster";
+            this.Text = "Magin of ett";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEtikett;
+        private System.Windows.Forms.TextBox tbxTal;
+        private System.Windows.Forms.Label lblSvar;
+        private System.Windows.Forms.Button btnKör;
+        private System.Windows.Forms.Label lblAddera1;
+        private System.Windows.Forms.Label lblMultiplicera2;
+        private System.Windows.Forms.Label lblSubtrahera6;
+        private System.Windows.Forms.Label lblHalvera;
+        private System.Windows.Forms.Label lblAddera3;
+        private System.Windows.Forms.Label lblSubtraheraTalet;
+        private System.Windows.Forms.Label lblFel;
+    }
+}
+
---
+++ Uppgifter/Uppgift_5.1/Uppgift_5.1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_5._1
+{
+    public partial class Fönster : Form
+    {
+        public Fönster()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKör_Click(object sender, EventArgs e)
+        {
+            int tal = fåTal(tbxTal.Text);
+            
+            int svar = tal;
+            if (svar >= 0)
+            {
+                lblAddera1.Text = svar + " + " + "1 = " + (svar += 1);
+                lblMultiplicera2.Text = svar + " * " + "2 = " + (svar *= 2);
+                lblSubtrahera6.Text = svar + " - " + " 6 = " + (svar -= 6);
+                lblHalvera.Text = svar + " / " + "2 = " + (svar /= 2);
+                lblAddera3.Text = svar + " + " + "3 = " + (svar += 3);
+                lblSubtraheraTalet.Text = svar + " - " + tal + "= " + (svar -= tal);
+
+                // svar = (((((tal + 1) * 2) - 6) / 2) + 3) - tal;
+                lblSvar.Text = "Svar: " + svar;
+            }
+            else
+            {
+                lblFel.Text = "Det var fel tallet.";
+            }
+          
+        }
+
+        private int fåTal(string text)
+        {
+            try
+            {
+                return int.Parse(text);
+            }
+            catch (FormatException)
+            {
+                double nyttTalDouble;
+
+                if (text.Contains("."))
+                {
+                    string nyttTalText = text.Replace('.', ',');
+                    nyttTalDouble = double.Parse(nyttTalText);
+                    return (int)nyttTalDouble;
+                }
+                 else if (tbxTal.Text.Contains(","))
+                 {
+                    nyttTalDouble = double.Parse(text);
+                    return (int)nyttTalDouble;
+                 }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+}
