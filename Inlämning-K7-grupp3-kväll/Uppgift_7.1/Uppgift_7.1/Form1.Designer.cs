d239afcf765549d206dedb0112fc026761d4025e
Geisy Domiciano <geisydomiciano@gmail.com>
2020-09-21 21:03:50 +0200

    :sparkles: skapa uppgift 7.1

---
+++ Inlämning-K7-grupp3-kväll/Uppgift_7.1/Uppgift_7.1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Uppgift_7._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTal = new System.Windows.Forms.Label();
+            this.tbxTal = new System.Windows.Forms.TextBox();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTal
+            // 
+            this.lblTal.AutoSize = true;
+            this.lblTal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTal.Location = new System.Drawing.Point(34, 39);
+            this.lblTal.Name = "lblTal";
+            this.lblTal.Size = new System.Drawing.Size(32, 17);
+            this.lblTal.TabIndex = 0;
+            this.lblTal.Text = "Tal:";
+            // 
+            // tbxTal
+            // 
+            this.tbxTal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxTal.Location = new System.Drawing.Point(80, 36);
+            this.tbxTal.Name = "tbxTal";
+            this.tbxTal.Size = new System.Drawing.Size(100, 23);
+            this.tbxTal.TabIndex = 1;
+            this.tbxTal.TextChanged += new System.EventHandler(this.tbxTal_TextChanged);
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.BackColor = System.Drawing.SystemColors.Control;
+            this.lblSvar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSvar.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblSvar.Location = new System.Drawing.Point(37, 78);
+            this.lblSvar.MinimumSize = new System.Drawing.Size(150, 20);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(150, 22);
+            this.lblSvar.TabIndex = 2;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(80, 119);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 186);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.tbxTal);
+            this.Controls.Add(this.lblTal);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Text = "Uppgift 7.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTal;
+        private System.Windows.Forms.TextBox tbxTal;
+        private System.Windows.Forms.Label lblSvar;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ Inlämning-K7-grupp3-kväll/Uppgift_7.1/Uppgift_7.1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_7._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbxTal_TextChanged(object sender, EventArgs e)
+        {
+            lblSvar.Text = "";
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            double tal = double.Parse(tbxTal.Text);
+            string svar = "Talet är ";
+            if(tal > 0)
+            {
+                svar += "positiv";
+            }
+            else if (tal < 0) {
+                svar += "negativ";
+            }
+            else
+            {
+                svar += "0";
+            }
+
+            lblSvar.Text = svar;
+        }
+    }
+}
