0fdf5aa7c7d2a5dff2cb45785bb568249da9324c
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-03 12:32:09 +0200

    :sparkles: skapa uppgift 9.3

---
+++ Inlämning-K9-grupp3-ikväll/Uppgift_9.3/Uppgift_9.3/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Uppgift_9._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSvar.Location = new System.Drawing.Point(31, 28);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(0, 17);
+            this.lblSvar.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 151);
+            this.Controls.Add(this.lblSvar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSvar;
+    }
+}
+
---
+++ Inlämning-K9-grupp3-ikväll/Uppgift_9.3/Uppgift_9.3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_9._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double a4Ark = 0.1;
+            double månenDistans = (double) 384000 * 1000000;
+
+            int räknare = 1;
+            while (a4Ark <= månenDistans){
+                a4Ark *= 2;
+                räknare++;
+            }
+
+            lblSvar.Text = "Du måste vika ett A4-ark " + räknare + "gånger \nför att tjokleken blir lika med avståndet till månen. \n\n A4-ark tjokleken är: " + (int)(a4Ark / 1000000) + "km";
+        }
+    }
+}
