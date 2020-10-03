f2b48844c1d603292a21d23c160d640d18aeb86e
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-03 15:10:43 +0200

    :skapa uppgift 9.5:

---
+++ Inlämning-K9-grupp3-ikväll/Uppgift_9.5/Uppgift_9.5/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_9._5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+            Pen penna;
+
+            for (int i = 255; i >= 0; i--)
+            {
+                penna = new Pen(Color.FromArgb(255, i, i));
+                g.DrawRectangle(penna, i, 0, 1, 255);
+            }
+        }
+    }
+}
