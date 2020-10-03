c7a60d8ca012b775c654a4b0aeb3b59bdfdf56ed
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-03 11:19:00 +0200

    :sparkles: skapa uppgift 9.1

---
+++ Inlämning-K9-grupp3-ikväll/Uppgift_9.1/Uppgift_9.1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Uppgift_9._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxStartAntal = new System.Windows.Forms.TextBox();
+            this.tbxTid = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBeräkna = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResultat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Startantal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tid";
+            // 
+            // tbxStartAntal
+            // 
+            this.tbxStartAntal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxStartAntal.Location = new System.Drawing.Point(96, 29);
+            this.tbxStartAntal.Name = "tbxStartAntal";
+            this.tbxStartAntal.Size = new System.Drawing.Size(266, 23);
+            this.tbxStartAntal.TabIndex = 2;
+            // 
+            // tbxTid
+            // 
+            this.tbxTid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxTid.Location = new System.Drawing.Point(96, 63);
+            this.tbxTid.Name = "tbxTid";
+            this.tbxTid.Size = new System.Drawing.Size(100, 23);
+            this.tbxTid.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(202, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "x10 minuter";
+            // 
+            // btnBeräkna
+            // 
+            this.btnBeräkna.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBeräkna.Location = new System.Drawing.Point(96, 109);
+            this.btnBeräkna.Name = "btnBeräkna";
+            this.btnBeräkna.Size = new System.Drawing.Size(171, 30);
+            this.btnBeräkna.TabIndex = 5;
+            this.btnBeräkna.Text = "Beräkna tillväxt";
+            this.btnBeräkna.UseVisualStyleBackColor = true;
+            this.btnBeräkna.Click += new System.EventHandler(this.btnBeräkna_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Slutantal";
+            // 
+            // lblResultat
+            // 
+            this.lblResultat.AutoSize = true;
+            this.lblResultat.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultat.Location = new System.Drawing.Point(92, 173);
+            this.lblResultat.MinimumSize = new System.Drawing.Size(270, 20);
+            this.lblResultat.Name = "lblResultat";
+            this.lblResultat.Size = new System.Drawing.Size(270, 20);
+            this.lblResultat.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 223);
+            this.Controls.Add(this.lblResultat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnBeräkna);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxTid);
+            this.Controls.Add(this.tbxStartAntal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxStartAntal;
+        private System.Windows.Forms.TextBox tbxTid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBeräkna;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResultat;
+    }
+}
+
---
+++ Inlämning-K9-grupp3-ikväll/Uppgift_9.1/Uppgift_9.1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_9._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBeräkna_Click(object sender, EventArgs e)
+        {
+            BigInteger resultat = BigInteger.Parse(fåTal(tbxStartAntal.Text).ToString());
+            int tid = fåTal(tbxTid.Text); 
+
+            if(tid > 0 && resultat > 0)
+            {
+                for(int i = 0; i < tid; i++) resultat *= 2; 
+
+                lblResultat.Text = resultat.ToString();
+            } else
+            {
+                lblResultat.Text = "indata var fel. försoka igen";
+            }
+        }
+
+        private int fåTal(string text)
+        {
+            try
+            {
+                return int.Parse(text);
+            }
+            catch (FormatException)
+            {
+                double nyttTalDouble;
+
+                if (text.Contains("."))
+                {
+                    string nyttTalText = text.Replace('.', ',');
+                    nyttTalDouble = double.Parse(nyttTalText);
+                    return (int)nyttTalDouble;
+                }
+                else if (text.Contains(","))
+                {
+                    nyttTalDouble = double.Parse(text);
+                    return (int)nyttTalDouble;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+}
