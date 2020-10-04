5957bebfc16a75d2827c122e5375ef4a476883a3
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-04 08:56:26 +0200

    :sparkles: addera uppgift 9.6

---
+++ Inlämning-K9-grupp3-ikväll/uppgift_9.6/uppgift_9.6/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uppgift_9._6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random slumpa = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sexorRäknare = 0;
+            for (int i = 0; i<=1000; i++)
+            {
+                int tärningsTal = slumpa.Next(1, 7);
+                if (tärningsTal == 6)
+                {
+                    tbxSexor.AppendText(tärningsTal+ "\t");
+                    sexorRäknare++;
+                }
+            }
+            tbxSexor.AppendText("\n\n\n Totalt: " + sexorRäknare);
+
+        }
+    }
+}
