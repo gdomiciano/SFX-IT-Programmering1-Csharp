96b3f76b000c08bcc4a47b6f575f537c3cb82940
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-05 21:12:44 +0200

    :construction:  skapa uppgift 10.2 design

---
+++ Inlämning-K10-grupp3-kväll/Uppgift_10.2/Uppgift_10.2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_10._2
+{
+    public partial class Uppgift_10_2 : Form
+    {
+        public Uppgift_10_2()
+        {
+            InitializeComponent();
+        }
+
+        char bestämTecken(double tal)
+        {
+
+        }
+    }
+}
