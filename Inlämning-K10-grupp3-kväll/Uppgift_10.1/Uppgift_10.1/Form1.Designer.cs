cd45c74287b4f03a89964ed90297832b0dc92665
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-05 21:05:03 +0200

    :construction:  skapa uppgift 10.1 design

---
+++ Inlämning-K10-grupp3-kväll/Uppgift_10.1/Uppgift_10.1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Uppgift_10._1
+{
+    partial class Uppgift_10_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.btnBeräkna = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radie: ";
+            // 
+            // tbx
+            // 
+            this.tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx.Location = new System.Drawing.Point(83, 13);
+            this.tbx.Name = "tbx";
+            this.tbx.Size = new System.Drawing.Size(100, 23);
+            this.tbx.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Omlrets: ";
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSvar.Location = new System.Drawing.Point(83, 67);
+            this.lblSvar.MinimumSize = new System.Drawing.Size(100, 0);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(100, 19);
+            this.lblSvar.TabIndex = 3;
+            // 
+            // btnBeräkna
+            // 
+            this.btnBeräkna.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBeräkna.Location = new System.Drawing.Point(16, 110);
+            this.btnBeräkna.Name = "btnBeräkna";
+            this.btnBeräkna.Size = new System.Drawing.Size(167, 23);
+            this.btnBeräkna.TabIndex = 4;
+            this.btnBeräkna.Text = "Beräkna omkrets";
+            this.btnBeräkna.UseVisualStyleBackColor = true;
+            // 
+            // Uppgift_10_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 163);
+            this.Controls.Add(this.btnBeräkna);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbx);
+            this.Controls.Add(this.label1);
+            this.Name = "Uppgift_10_1";
+            this.Text = "Uppgift 10.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSvar;
+        private System.Windows.Forms.Button btnBeräkna;
+    }
+}
+
---
+++ Inlämning-K10-grupp3-kväll/Uppgift_10.1/Uppgift_10.1/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_10._1
+{
+    public partial class Uppgift_10_1 : Form
+    {
+        public Uppgift_10_1()
+        {
+            InitializeComponent();
+        }
+        double omkretsCirkel (double radie)
+        {
+
+        }
+    }
+}
