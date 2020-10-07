f9872fb4e166ee25e2431fd2a2f08cd0035c3258
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-07 18:48:51 +0200

    :construction: skapa uppgift 10.5 design

---
+++ Inlämning-K10-grupp3-kväll/Uppgift_10.5/Uppgift_10.5/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Uppgift_10._5
+{
+    partial class Uppgift_10_5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxGemen = new System.Windows.Forms.TextBox();
+            this.lblVerssal = new System.Windows.Forms.Label();
+            this.btnVisa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gemen:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Versal:";
+            // 
+            // tbxGemen
+            // 
+            this.tbxGemen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxGemen.Location = new System.Drawing.Point(115, 13);
+            this.tbxGemen.Name = "tbxGemen";
+            this.tbxGemen.Size = new System.Drawing.Size(100, 23);
+            this.tbxGemen.TabIndex = 2;
+            // 
+            // lblVerssal
+            // 
+            this.lblVerssal.AutoSize = true;
+            this.lblVerssal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblVerssal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVerssal.Location = new System.Drawing.Point(115, 62);
+            this.lblVerssal.MinimumSize = new System.Drawing.Size(100, 0);
+            this.lblVerssal.Name = "lblVerssal";
+            this.lblVerssal.Size = new System.Drawing.Size(100, 19);
+            this.lblVerssal.TabIndex = 3;
+            // 
+            // btnVisa
+            // 
+            this.btnVisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVisa.Location = new System.Drawing.Point(16, 113);
+            this.btnVisa.Name = "btnVisa";
+            this.btnVisa.Size = new System.Drawing.Size(199, 23);
+            this.btnVisa.TabIndex = 4;
+            this.btnVisa.Text = "VIsa versal";
+            this.btnVisa.UseVisualStyleBackColor = true;
+            // 
+            // Uppgift_10_5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 154);
+            this.Controls.Add(this.btnVisa);
+            this.Controls.Add(this.lblVerssal);
+            this.Controls.Add(this.tbxGemen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Uppgift_10_5";
+            this.Text = "Uppgift 10.5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxGemen;
+        private System.Windows.Forms.Label lblVerssal;
+        private System.Windows.Forms.Button btnVisa;
+    }
+}
+
