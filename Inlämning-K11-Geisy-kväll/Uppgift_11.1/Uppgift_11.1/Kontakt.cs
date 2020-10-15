25ab1b57a830f80f7298e001ccb9b1bfabf06807
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-15 18:46:55 +0200

    :construction: skapa design och klassen kontakt

---
+++ Inlämning-K11-Geisy-kväll/Uppgift_11.1/Uppgift_11.1/Kontakt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_11._1
+{
+    class Kontakt
+    {
+    }
+}
