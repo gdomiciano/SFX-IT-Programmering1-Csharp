6686a9a60f42f69d871bba32038c77ec3d4d5f6f
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-15 18:48:27 +0200

    :construction: skapa klassen kontakt

--- Inlämning-K11-Geisy-kväll/Uppgift_11.1/Uppgift_11.1/Kontakt.cs
+++ Inlämning-K11-Geisy-kväll/Uppgift_11.1/Uppgift_11.1/Kontakt.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Uppgift_11._1
 {
-    class Kontakt
+    public class Kontakt
     {
+        public string förnamn { get; set; }
+        public string efternamn { get; set; }
+        public string telefonnummer { get; set; }
+
+        public Kontakt(string förnamn, string efternamn, string telefonnummer)
+        {
+            this.förnamn = förnamn;
+            this.efternamn = efternamn;
+            this.telefonnummer = telefonnummer;
+        }
     }
 }
