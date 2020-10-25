ef6c4d996a9710289f767e675cdadb83ba2796d6
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-25 15:47:53 +0100

    :tada::sparkles: skapa uppgift 12.1

---
+++ Inlämning-K12-grupp1-kväll/Uppgift_12.1/Uppgift_12.1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Uppgift_12._1
+{
+    partial class Sortering
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tal1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxTal1 = new System.Windows.Forms.TextBox();
+            this.tbxTal2 = new System.Windows.Forms.TextBox();
+            this.tbxTal3 = new System.Windows.Forms.TextBox();
+            this.tbxTal4 = new System.Windows.Forms.TextBox();
+            this.tbxTal5 = new System.Windows.Forms.TextBox();
+            this.btnStigande = new System.Windows.Forms.Button();
+            this.btnFallande = new System.Windows.Forms.Button();
+            this.btnÅterställa = new System.Windows.Forms.Button();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Tal1
+            // 
+            this.Tal1.AutoSize = true;
+            this.Tal1.Location = new System.Drawing.Point(79, 30);
+            this.Tal1.Name = "Tal1";
+            this.Tal1.Size = new System.Drawing.Size(44, 17);
+            this.Tal1.TabIndex = 0;
+            this.Tal1.Text = "Tal 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tal 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tal 3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tal 4:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tal 5:";
+            // 
+            // tbxTal1
+            // 
+            this.tbxTal1.Location = new System.Drawing.Point(129, 27);
+            this.tbxTal1.Name = "tbxTal1";
+            this.tbxTal1.Size = new System.Drawing.Size(90, 22);
+            this.tbxTal1.TabIndex = 5;
+            // 
+            // tbxTal2
+            // 
+            this.tbxTal2.Location = new System.Drawing.Point(129, 55);
+            this.tbxTal2.Name = "tbxTal2";
+            this.tbxTal2.Size = new System.Drawing.Size(90, 22);
+            this.tbxTal2.TabIndex = 6;
+            // 
+            // tbxTal3
+            // 
+            this.tbxTal3.Location = new System.Drawing.Point(129, 83);
+            this.tbxTal3.Name = "tbxTal3";
+            this.tbxTal3.Size = new System.Drawing.Size(90, 22);
+            this.tbxTal3.TabIndex = 7;
+            // 
+            // tbxTal4
+            // 
+            this.tbxTal4.Location = new System.Drawing.Point(129, 111);
+            this.tbxTal4.Name = "tbxTal4";
+            this.tbxTal4.Size = new System.Drawing.Size(90, 22);
+            this.tbxTal4.TabIndex = 8;
+            // 
+            // tbxTal5
+            // 
+            this.tbxTal5.Location = new System.Drawing.Point(129, 139);
+            this.tbxTal5.Name = "tbxTal5";
+            this.tbxTal5.Size = new System.Drawing.Size(90, 22);
+            this.tbxTal5.TabIndex = 9;
+            // 
+            // btnStigande
+            // 
+            this.btnStigande.Location = new System.Drawing.Point(17, 187);
+            this.btnStigande.Name = "btnStigande";
+            this.btnStigande.Size = new System.Drawing.Size(99, 46);
+            this.btnStigande.TabIndex = 10;
+            this.btnStigande.Text = "Sortera stigande";
+            this.btnStigande.UseVisualStyleBackColor = true;
+            this.btnStigande.Click += new System.EventHandler(this.btnStigande_Click);
+            // 
+            // btnFallande
+            // 
+            this.btnFallande.Location = new System.Drawing.Point(122, 187);
+            this.btnFallande.Name = "btnFallande";
+            this.btnFallande.Size = new System.Drawing.Size(102, 46);
+            this.btnFallande.TabIndex = 11;
+            this.btnFallande.Text = "Sortera fallande";
+            this.btnFallande.UseVisualStyleBackColor = true;
+            this.btnFallande.Click += new System.EventHandler(this.btnFallande_Click);
+            // 
+            // btnÅterställa
+            // 
+            this.btnÅterställa.Location = new System.Drawing.Point(230, 199);
+            this.btnÅterställa.Name = "btnÅterställa";
+            this.btnÅterställa.Size = new System.Drawing.Size(75, 23);
+            this.btnÅterställa.TabIndex = 12;
+            this.btnÅterställa.Text = "Återställa";
+            this.btnÅterställa.UseVisualStyleBackColor = true;
+            this.btnÅterställa.Click += new System.EventHandler(this.btnÅterställa_Click);
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSvar.Location = new System.Drawing.Point(12, 256);
+            this.lblSvar.MinimumSize = new System.Drawing.Size(300, 20);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(300, 20);
+            this.lblSvar.TabIndex = 13;
+            // 
+            // Sortering
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 299);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.btnÅterställa);
+            this.Controls.Add(this.btnFallande);
+            this.Controls.Add(this.btnStigande);
+            this.Controls.Add(this.tbxTal5);
+            this.Controls.Add(this.tbxTal4);
+            this.Controls.Add(this.tbxTal3);
+            this.Controls.Add(this.tbxTal2);
+            this.Controls.Add(this.tbxTal1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Tal1);
+            this.Name = "Sortering";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Tal1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxTal1;
+        private System.Windows.Forms.TextBox tbxTal2;
+        private System.Windows.Forms.TextBox tbxTal3;
+        private System.Windows.Forms.TextBox tbxTal4;
+        private System.Windows.Forms.TextBox tbxTal5;
+        private System.Windows.Forms.Button btnStigande;
+        private System.Windows.Forms.Button btnFallande;
+        private System.Windows.Forms.Button btnÅterställa;
+        private System.Windows.Forms.Label lblSvar;
+    }
+}
+
---
+++ Inlämning-K12-grupp1-kväll/Uppgift_12.1/Uppgift_12.1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift_12._1
+{
+    public partial class Sortering : Form
+    {
+        public Sortering()
+        {
+            InitializeComponent();
+        }
+        List<int> talLista = new List<int>();
+
+        private void skapaFält ()
+        {
+            talLista.Clear();
+            try
+            {
+                talLista.Add(int.Parse(tbxTal1.Text));
+                talLista.Add(int.Parse(tbxTal2.Text));
+                talLista.Add(int.Parse(tbxTal3.Text));
+                talLista.Add(int.Parse(tbxTal4.Text));
+                talLista.Add(int.Parse(tbxTal5.Text));
+            }
+            catch (FormatException)
+            {
+                lblSvar.Text = "det var en fel";
+            }
+        }
+
+        private void btnStigande_Click(object sender, EventArgs e)
+        {
+
+            skapaFält();
+
+            int i, n;
+            int lenght = talLista.Count; if (lenght < 2) return;
+            int temp;
+
+            for (n = 1; n < lenght; n++)
+            {
+                temp = talLista[n];
+                i = n - 1;
+
+                while (i >= 0 && talLista[i] < temp)
+                {
+                    talLista[i + 1] = talLista[i];
+                    i--;
+                }
+
+                talLista[i + 1] = temp;
+            }
+
+            skrivaSortering();
+        }
+
+        private void btnFallande_Click(object sender, EventArgs e)
+        {
+            skapaFält();
+
+            int i, n;
+            int lenght = talLista.Count; if (lenght < 2) return;
+            int temp;
+
+            for (n = 1; n<lenght; n++)
+            {
+                temp = talLista[n];
+                i = n - 1;
+
+                while (i >= 0 && talLista[i] > temp)
+                {
+                    talLista[i + 1] = talLista[i];
+                    i--;
+                }
+
+                talLista[i + 1] = temp;
+            }
+
+            skrivaSortering();
+        }
+
+        private void skrivaSortering()
+        {
+            lblSvar.Text = "";
+
+            for(int i = 0; i < talLista.Count; i++)
+            {
+                lblSvar.Text += talLista[i].ToString() + ", ";
+            }
+        }
+
+        private void btnÅterställa_Click(object sender, EventArgs e)
+        {
+            tbxTal1.Clear();
+            tbxTal2.Clear();
+            tbxTal3.Clear();
+            tbxTal4.Clear();
+            tbxTal5.Clear();
+            lblSvar.Text = "";
+            talLista.Clear();
+        }
+    }
+}
