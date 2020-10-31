89b870d60cfdb957a378dedb50a2e80428f23e5b
Geisy Domiciano <geisydomiciano@gmail.com>
2020-10-31 15:32:05 +0100

    :sparkles: klart med uppgift 14.1

--- Inlämning-K14-grupp1-kväll/Uppgift_14.1/Uppgift_14.1/Form1.cs
+++ Inlämning-K14-grupp1-kväll/Uppgift_14.1/Uppgift_14.1/Form1.cs
@@ -12,9 +12,32 @@ namespace Uppgift_14._1
 {
     public partial class Uppgift_14_1 : Form
     {
+        int börja = 8000;
+        double rämta = 1.05;
         public Uppgift_14_1()
         {
             InitializeComponent();
+        }
+
+        private void btnVisa_Click(object sender, EventArgs e)
+        {
+            int år = int.Parse(tbxÅr.Text);
+            int resultat = FåKapitalet(år);
+            lblResultat.Text = $"Kepitelet är {resultat} kr";
+        }
+
+        private int FåKapitalet(int år)
+        {
+            int k = börja;
+            if (år == 0)
+            {
+                return k;
+            }
+            else
+            {
+                k = (int)(rämta * FåKapitalet(år - 1));
+            }
+            return k;
         }
     }
 }
