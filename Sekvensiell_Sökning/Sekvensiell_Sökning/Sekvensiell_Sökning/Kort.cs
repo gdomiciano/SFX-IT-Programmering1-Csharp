b8cfde1f05fa161c572dc3393b6e0b0e5714a94a
Geisy Domiciano <geisydomiciano@gmail.com>
2020-11-02 20:36:00 +0100

    :construction: skapa klasser

---
+++ Sekvensiell_Sökning/Sekvensiell_Sökning/Sekvensiell_Sökning/Kort.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sekvensiell_Sökning
+{
+    class Kort
+    {
+
+        // Kortets x-läge
+        private int x;
+        // Kortets y-läge
+        private int y;
+        // Kortets valör
+        private int valör;
+        // Kortets bredd
+        private int bredd = 50;
+        // Kortets höjd
+        private int höjd = 70;
+        // Bestämmer om kortets fram eller baksida ska ritas
+        private bool visaFramsida = true;
+        /// <summary>
+        /// Egenskap för att sätta/hämta kortets x-läge
+        /// </summary>
+        public int X
+        {
+        }
+        /// <summary>
+        /// Egenskap för att sätta/hämta kortets y-läge
+        /// </summary>
+        public int Y
+        {
+        }
+        /// <summary>
+        /// Egenskap för att sätta/hämta kortets valör
+        /// </summary>
+        public int Valör
+        {
+        }
+        /// <summary>
+        /// Egenskap för att hämta kortets bredd
+        /// </summary>
+        public int Bredd
+        {
+        }
+        /// <summary>
+        /// Egenskap för att hämta kortets höjd
+        /// </summary>
+        public int Höjd
+        {
+        }
+        /// <summary>
+        /// Egenskap för att sätta/hämta om framsidan ska visas eller inte
+        /// </summary>
+        public bool VisaFramsida
+        {
+        }
+        /// <summary>
+        /// Konstruktor som initierar ett kort
+        /// </summary>
+        /// <param name="x">Kortets x-läge</param>
+        /// <param name="y">kortets y-läge</param>
+        /// <param name="valör">Kortets valör</param>
+        public Kort(int x, int y, int valör)
+        {
+        }
+        /// <summary>
+        /// Ritar ett ruterkort. En enda rutersymbol ritas i mitten av kortet.
+        /// </summary>
+        /// <param name="g"></param>
+        public void Rita(Graphics g)
+        {
+            if (VisaFramsida)
+            {
+                // Kortets framsida ritas
+                // Rita en vit framsida
+
+                // Rita en rutersymbol i mitten
+                // Skriv valören i hörnen
+            }
+            else
+            {
+                // Ritar baksidan som en röd rektangel
+            }
+        }
+    }
+}
---
+++ Sekvensiell_Sökning/Sekvensiell_Sökning/Sekvensiell_Sökning/Kortlek.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sekvensiell_Sökning
+{
+    class Kortlek
+    {
+
+        // Slumptalsgenerator används för att blanda kortleken
+        Random slump = new Random();
+        // Antalet kort i kortbunten
+        private static int antalKort = 9;
+        // Korten i kortbunten
+        private Kort[] korten = new Kort[antalKort];
+        // Index för for-loopen vid sekventiell sökning
+        private int iFor = 0;
+        // Index för det sökta kortet.
+        private int index = -1;
+        // Avbryt sökningen för att kortet har hittats eller för att
+        // hela kortbunten har sökts igenom.
+        private bool avbryt = false;
+        /// <summary>
+        /// Konstruktor som skapar en kortbunt. Kortbunten är sorterad
+        /// och det kort med lägst värde ligger längst till vänster.
+        /// </summary>
+ public Kortlek()
+        {
+        }
+        /// <summary>
+        /// Blandar kortleken genom att stega igenom den och byta plats på två kort.
+        /// Korten vänds sedan med framsidan nedåt.
+        /// </summary>
+        public void Blanda()
+        {
+            // Blanda kortbunten
+            // Visa kortbuntens baksida
+            // Initiera variablerna som hanterar sekventiell sökning
+            iFor = 0;
+            index = -1;
+            avbryt = false;
+        }
+        // Ger index för det sökta kortet i kortbunten.
+        public int Index
+        {
+        }
+        /// <summary>
+        /// Söker efter ett kort med en viss valör i kortbunten. Metoden
+        /// som används är sekventiell sökning. När kortet har hittats returneras
+        /// true.
+        /// </summary>
+        public bool SekventielltSökSteg(int söktValör)
+        {
+        }
+        /// <summary>
+        /// Ritar korten i kortbunten.
+        /// </summary>
+        /// <param name="g"></param>
+        public void Rita(Graphics g)
+        {
+        }
+    }
+}
